47920e6ee6aada7ecbcea96e91c4836ee1552529
Nitish <nitishmankeshvar7@gmail.com>
2020-09-23 20:42:00 +0530

    disclamers

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+    **************************@copyright 2020*******************************
+
+    Gamers API Version 1
+    Author: Nitish Mankeshvar
+    This API is the sole property of the author and the use of this api 
+    other than the author must be first with the concent of him respectfully.
+
+    ************************************************************************
+*/
