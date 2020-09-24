40d36edac8296f3a65409678ec1ed3ddcf0ed2e0
Nitish <nitishmankeshvar7@gmail.com>
2020-09-24 22:13:55 +0530

    added mvc folders and controller methods

---
+++ controllers/gamersControllers.js
@@ -0,0 +1,39 @@
+// MEHTOD         : GET
+// ROUTE          : api/v1/gamers
+// AUTHENTICATION : public
+exports.getGamers = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: "Get all gamers",
+  });
+};
+
+// MEHTOD         : GET
+// ROUTE          : api/v1/gamers/:id
+// AUTHENTICATION : public
+exports.getSingleGamer = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: "Get specific gamer with his id",
+  });
+};
+
+// MEHTOD         : DELETE
+// ROUTE          : api/v1/gamers/:id
+// AUTHENTICATION : public
+exports.deleteGamer = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: `DELETE ${req.params.id} specific gamer with his id`,
+  });
+};
+
+// MEHTOD         : PUT
+// ROUTE          : api/v1/gamers/:id
+// AUTHENTICATION : public
+exports.updateGamer = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: `UPDATE ${req.params.id} specific gamer with his id`,
+  });
+};
--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 /*
     **************************@copyright 2020*******************************
 
-    Gamers API Version 1
-    Author: Nitish Mankeshvar
+    Gamers API Version 1.0
+    Author: Nitish Mankeshvar (nvision studios)
     This API is the sole property of the author and the use of this api 
     other than the author must be first with the concent of him respectfully.
 
     ************************************************************************
 */
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    res: "hi",
+  });
+});
+
+app.listen(5000, () => console.log("server started!"));
