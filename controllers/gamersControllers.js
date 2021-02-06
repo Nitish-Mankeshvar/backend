953117dd656cae59a00eb3ad9562f56b27fd8cbb
Nitish <nitishmankeshvar7@gmail.com>
2021-02-06 15:05:05 +0530

    completed backend

--- controllers/gamersControllers.js
+++ controllers/gamersControllers.js
@@ -9,7 +9,7 @@ exports.createGamer = async (req, res, next) => {
     const user = await GamerModel.create(req.body);
     res.status(200).json({
       success: true,
-      data: req.body,
+      data: user,
     });
   } catch (error) {
     res.status(200).json({
